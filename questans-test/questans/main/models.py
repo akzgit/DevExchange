74e03d8992121f84ab36fda6ea06a2774d94c419
akzgit <akashtyo@gmail.com>
2023-09-26 17:00:16 +0530

    26-09-2023

--- questans-test/questans/main/models.py
+++ questans-test/questans/main/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User,AbstractUser
+
 
 
 # Question Model
@@ -20,3 +21,23 @@ class Answer(models.Model):
 
     def __str__(self):
         return self.detail
+
+# Comment
+class Comment(models.Model):
+    answer=models.ForeignKey(Answer,on_delete=models.CASCADE)
+    user=models.ForeignKey(User,on_delete=models.CASCADE,related_name='comment_user')
+    add_time=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.comment
+
+# UpVotes
+class UpVote(models.Model):
+    answer=models.ForeignKey(Answer,on_delete=models.CASCADE)
+    user=models.ForeignKey(User,on_delete=models.CASCADE,related_name='upvote_user')
+
+# DownVotes
+class DownVote(models.Model):
+    answer=models.ForeignKey(Answer,on_delete=models.CASCADE)
+    user=models.ForeignKey(User,on_delete=models.CASCADE,related_name='downvote_user')
+
